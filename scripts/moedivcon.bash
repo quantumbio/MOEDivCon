c71646237d03405e9a1517d5f00df617d9ecd714
daren <daren@0241862a-156b-41b8-859c-7af79c85dedb>
2009-10-09 18:50:42 +0000

    Created PBS and SGE scripts to run moebatch
    git-svn-id: https://svn.quantumbioinc.com/MOEDivcon/trunk@4490 0241862a-156b-41b8-859c-7af79c85dedb

---
+++ scripts/moedivcon.bash
@@ -0,0 +1,8 @@
+# You may remove those variables that are already properly defined in 
+# your shell record (.bashrc).
+
+# Change CHEMIXDIR to point to your local installation.
+export CHEMIXDIR=$HOME/playground/MOEDivcon
+export PATH=$CHEMIXDIR/bin:$PATH
+export MOE_SVL_LOAD=$CHEMIXDIR/svl
+export LD_LIBRARY_PATH=$CHEMIXDIR/lib:$LD_LIBRARY_PATH
