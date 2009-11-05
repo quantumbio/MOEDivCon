381f485edb6dee72f9fe9337fa06fe96c551947f
daren <daren@0241862a-156b-41b8-859c-7af79c85dedb>
2009-11-05 23:15:01 +0000

    Added headers to svl files; refactored to use MOE_SVL_RUNPATH
    git-svn-id: https://svn.quantumbioinc.com/MOEDivcon/trunk@4767 0241862a-156b-41b8-859c-7af79c85dedb

--- scripts/moedivcon.bash
+++
@@ -1,8 +0,0 @@
-# You may remove those variables that are already properly defined in 
-# your shell record (.bashrc).
-
-# Change QBHOME to point to your local installation.
-export QBHOME=$HOME/playground/MOEDivcon
-export PATH=$QBHOME/bin:$PATH
-export MOE_SVL_LOAD=$QBHOME/svl
-export LD_LIBRARY_PATH=$QBHOME/lib:$LD_LIBRARY_PATH
