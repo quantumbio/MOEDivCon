2794bebe5584cc30e38f7d0dc25e60b804931660
daren <daren@0241862a-156b-41b8-859c-7af79c85dedb>
2010-12-13 21:45:40 +0000

    Split dbtest into run then test.
    git-svn-id: https://svn.quantumbioinc.com/MOEDivcon/trunk@6616 0241862a-156b-41b8-859c-7af79c85dedb

--- functests/dbdiff.py
+++ functests/dbdiff.py
@@ -1,72 +1,43 @@
 #!/usr/bin/env python
 #
-# This script performs two major functions:
-#   1) Submit all of the PBS jobs in newdir and wait for them to complete.
-#   2) Compare the output for correctness.
+#   Compare the output for correctness.
 #
 # Created on 2010-12-07 by Scott Woods <scott@westarete.com> for QuantumBio Inc.
 
-import os.path, re, subprocess, shlex, string, sys, time
+import os.path, re, subprocess, shlex, sys
 from glob import glob
-from pprint import pprint
-from sets import Set
 
 if len(sys.argv) != 3:
-	print "Usage: ./dbdiff.py basedir newdir"
-	sys.exit(1)
+    print "Usage: ./dbdiff.py basedir newdir"
+    sys.exit(1)
 basedir = sys.argv[1]
 newdir  = sys.argv[2]
 
-# Submit each job.
-submitted_job_ids = set([])
-for scriptfile in glob(newdir + '/*.pbs'):
-	job_id = string.strip(subprocess.Popen(['qsub', scriptfile], stdout=subprocess.PIPE).communicate()[0])
-	print 'Submitted job ' + job_id
-	submitted_job_ids.add(job_id)
-
-# Wait until each job completes.
-started_at = time.clock()
-completed_job_ids = set([])
-while submitted_job_ids != completed_job_ids:
-	time.sleep(5)
-	for job_id in submitted_job_ids - completed_job_ids:
-		retval = subprocess.call(['qstat', job_id], stdout=open('/dev/null', 'w'))
-		if retval > 0:
-			print 'Job ' + job_id + ' is no longer running'
-			completed_job_ids.add(job_id)
-	hour_limit = 12
-	if submitted_job_ids != completed_job_ids and time.clock() - started_at > 60*60*hour_limit:
-		print "Fatal: Waited " + hour_limit + " hours for all jobs to complete."
-		print "Here are the ones that are still running:"
-		for job_id in submitted_job_ids - completed_job_ids:
-			print "    " + job_id
-print 'All jobs are complete'
-
 # A function to run a command and check the return value.
 def run(cmd):
-	print "Running command: " + cmd
-	retval = subprocess.call(shlex.split(cmd))
-	if retval != 0:
-		raise Exception("command returned " + retval)
+    print "Running command: " + cmd
+    retval = subprocess.call(shlex.split(cmd))
+    if retval != 0:
+        raise Exception("command returned " + retval)
 
 # Check the results for correctness.
 for basefile in glob(basedir + '/*.mdb'):
-        filename = os.path.basename(basefile)
-        if re.search("pwd", filename):
-		testpwd = '1'
-	else: 
-		testpwd = '0'
-	newfile = newdir + '/' + filename
-	if not os.path.exists(newfile):
-		raise Exception('Fatal: "' + newfile + '" does not exist')
-	if testpwd:
-		run('moebatch -exec "run [\'pwdbatch.svl\', [\'' + newfile + '\', \'QMScore\', 15]]" -exit')
-	run('moebatch -exec "run [\'qbmdbdiff.svl\', [\'qbmdbdiff-error.log\', \'' + basefile + '\', \'' + newfile + '\', \'all\', 0.5, ' + testpwd + ']]" -exit')
+    filename = os.path.basename(basefile)
+    if re.search("pwd", filename):
+        testpwd = '1'
+    else: 
+        testpwd = '0'
+    newfile = newdir + '/' + filename
+    if not os.path.exists(newfile):
+        raise Exception('Fatal: "' + newfile + '" does not exist')
+    if testpwd:
+        run('moebatch -exec "run [\'pwdbatch.svl\', [\'' + newfile + '\', \'QMScore\', 15]]" -exit')
+    run('moebatch -exec "run [\'qbmdbdiff.svl\', [\'qbmdbdiff-error.log\', \'' + basefile + '\', \'' + newfile + '\', \'all\', 0.5, ' + testpwd + ']]" -exit')
 output_file = "qbmdbdiff-error.log"
 if not os.path.exists(output_file):
-	print "Output file does not exist. Something went wrong with the comparison."
+    print "Output file does not exist. Something went wrong with the comparison."
 if os.path.getsize(output_file) == 0:
-	print "Output file is empty. Databases are the same."
+    print "Output file is empty. Databases are the same."
 else:
-	print "Databases differ. See https://ci.quantumbioinc.com/job/MOEDivconIntegrationTests/ws/MOEDivcon/functests/qbmdbdiff-error.log for details."
-	sys.exit(1)
+    print "Databases differ. See https://ci.quantumbioinc.com/job/MOEDivconIntegrationTests/ws/MOEDivcon/functests/qbmdbdiff-error.log for details."
+    sys.exit(1)
---
+++ functests/runjobs.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+#
+#   Submit all PBS jobs in current directory and wait for them to complete.
+#
+# Created on 2010-12-07 by Scott Woods <scott@westarete.com> for QuantumBio Inc.
+
+import subprocess, string, sys, time
+from glob import glob
+
+# Submit each job.
+submitted_job_ids = set([])
+for scriptfile in glob('*.pbs'):
+    print 'job ' + scriptfile
+    job_id = string.strip(subprocess.Popen(['qsub', scriptfile], stdout=subprocess.PIPE).communicate()[0])
+    print 'Submitted job ' + job_id
+    submitted_job_ids.add(job_id)
+
+# Wait until each job completes.
+started_at = time.clock()
+completed_job_ids = set([])
+while submitted_job_ids != completed_job_ids:
+    time.sleep(5)
+    for job_id in submitted_job_ids - completed_job_ids:
+        retval = subprocess.call(['qstat', job_id], stdout=open('/dev/null', 'w'))
+        if retval > 0:
+            print 'Job ' + job_id + ' is no longer running'
+            completed_job_ids.add(job_id)
+    hour_limit = 12
+    if submitted_job_ids != completed_job_ids and time.clock() - started_at > 60*60*hour_limit:
+        print "Fatal: Waited " + hour_limit + " hours for all jobs to complete."
+        print "Here are the ones that are still running:"
+        for job_id in submitted_job_ids - completed_job_ids:
+            print "    " + job_id
+print 'All jobs are complete'
