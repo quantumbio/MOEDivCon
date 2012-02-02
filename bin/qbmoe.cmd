bf24d0a5059f6320cee9f50ab9b28241f8e30e46
lance <lance@0241862a-156b-41b8-859c-7af79c85dedb>
2012-02-02 19:18:23 +0000

    first attempt at substituting correct path in installer
    git-svn-id: https://svn.quantumbioinc.com/MOEDivcon/trunk@8177 0241862a-156b-41b8-859c-7af79c85dedb

--- bin/qbmoe.cmd
+++ bin/qbmoe.cmd
@@ -1,6 +1,7 @@
 set MOE=C:\moe2011
 set PLTFM_VER=windows-x86_64
-set WD=C:\testing-grounds\quantumbio
+rem set WD=C:\testing-grounds\quantumbio
+set WD=IN_INSTALLDIR
 cd %CD%
 set PATH=%WD%\bin;%MOE%\bin-i4w9;%WD%\%PLTFM_VER%\bin;%WD%\%PLTFM_VER%\lib;C:\Windows\SysWOW64;%PATH%
 
