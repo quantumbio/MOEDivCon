695b4003c84d1e306f21f2aa71998ae3113ba241
roger <roger@0241862a-156b-41b8-859c-7af79c85dedb>
2013-03-07 19:09:55 +0000

    Send densities, eigenvectors and energy levels to moe.  Added unit test
    git-svn-id: https://svn.quantumbioinc.com/MOEDivcon/trunk@9074 0241862a-156b-41b8-859c-7af79c85dedb

---
+++ test/HDF5Tests.java
@@ -0,0 +1,63 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import svljava.SVLJavaException;
+import svljava.SVLVar;
+
+/**
+ *
+ * @author roger
+ */
+public class HDF5Tests {
+    
+         static {
+         System.loadLibrary("jhdf5");
+         }
+    public HDF5Tests() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+     @Test
+    public void retrieveEigenData() {
+         try {
+             HDF5Correspondent hdf5Correspondent = new HDF5Correspondent();
+             SVLVar cmd = new SVLVar("retrieveEnergyLevels", true);
+//             cmd.
+             //tokens[1]="s59.pm6.sp";
+             SVLVar args = new SVLVar();
+             args.=new String[]{"/home/roger/testing-grounds/s59/Moe interface/s59.pm6.sp.xml.h5","s59.pm6.sp"});
+             
+             SVLVar[] arg = new SVLVar[2];
+             arg[0]=cmd;
+             arg[1]=new SVLVar(new String[]{"/home/roger/testing-grounds/s59/Moe interface/s59.pm6.sp.xml.h5","s59.pm6.sp"});
+             java.lang.System.out.println(arg[1].length()+" "+arg[1].getTokns()[1]);
+             int tid = 0;
+             SVLVar results = hdf5Correspondent.run(new SVLVar(arg), tid);
+         } catch (SVLJavaException sVLJavaException) {
+             java.lang.System.out.println(sVLJavaException.getMessage());
+         }
+     }
+}
