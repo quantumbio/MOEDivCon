334c9672f060176d15763e2a791f9082d3c3e4f6
roger <roger@0241862a-156b-41b8-859c-7af79c85dedb>
2013-11-21 17:32:35 +0000

    Made duples return an error code in the second slot
    git-svn-id: https://svn.quantumbioinc.com/MOEDivcon/trunk@9601 0241862a-156b-41b8-859c-7af79c85dedb

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ function pause(){
 
 date
 
-export PROJECT_PATH=/home/roger/DivConDiscoverySuite-b1741
+export PROJECT_PATH=/home/roger/DivConDiscoverySuite-b1761
 #export JAVA_HOME=/home/roger/jdk1.7.0_25
 #export ANT_HOME=/home/roger/apache/apache-ant-1.8.4
 #export OOBACKBONE_HOME=/home/roger/NetBeansProjects/OOBackbone
@@ -13,6 +13,8 @@ export PROJECT_PATH=/home/roger/DivConDiscoverySuite-b1741
 #export QMECHANIC_HOME=/home/roger/DivConDiscoverySuite-b1418
 
 export PATH=$PROJECT_PATH/bin:$PATH
+#qbmoebatch -exec "qbTesth5Main['/home/roger/testing-grounds/Lance/1LRI/1LRI-NMR.h5']" -exit
+
 export LD_LIBRARY_PATH=/home/roger/Software/hdf-java-2.9/lib/linux:$QMECHANIC_HOME/linux-x86_64/lib:/share/apps/intel/linux/composer_xe_2013.5.192/mkl/lib/intel64:/share/apps/intel/linux/composer_xe_2013.5.192/compiler/lib/intel64:/home/roger/NetBeansProjects/openmpi/lib:/home/roger/NetBeansProjects/antlr/lib:/home/roger/NetBeansProjects/boost/lib:/home/roger/NetBeansProjects/lmx/lib:/home/roger/NetBeansProjects/hdf5/lib:/home/roger/NetBeansProjects/log4cplus/lib:/home/roger/NetBeansProjects/icu4c/lib:/home/roger/NetBeansProjects/zlib/lib:/home/roger/NetBeansProjects/bzip2/lib:/home/roger/NetBeansProjects/FockianIntegrals/dist/Release/GNU-Linux-x86:/home/roger/NetBeansProjects/OOBackbone/dist/Release/GNU-Linux-x86:/home/roger/NetBeansProjects/JPRoothaan/dist/Release/GNU-Linux-x86:/home/roger/NetBeansProjects/QBPersistence/dist/Release/GNU-Linux-x86:/home/roger/NetBeansProjects/JNIDivcon/native/JNIDivcon/dist/Release/GNU-Linux-x86:/opt/gcc/current/lib64:/lib64:$LD_LIBRARY_PATH
 #export LD_LIBRARY_PATH=/home/roger/NetBeansProjects/icu4c/lib:/opt/gcc/current/lib64:/lib64:$LD_LIBRARY_PATH
 #export CLASSPATH=`pwd`/dist/MOEDivcon.jar:$QMECHANIC_HOME/linux-x86_64/HDFView.jar:$QMECHANIC_HOME/linux-x86_64/QBReporter.jar:/share/apps/MOE/MOE-2012/java/svljava.jar:$CLASSPATH
@@ -21,7 +23,7 @@ export LD_LIBRARY_PATH=/home/roger/Software/hdf-java-2.9/lib/linux:$QMECHANIC_HO
 #ant test-single -DOPAL_PREFIX=$OPAL_PREFIX -DQMECHANIC_HOME=$QMECHANIC_HOME -Djava.library.path=$LD_LIBRARY_PATH -Dtest.includes=HDF5Tests.java -Djavac.includes=HDF5Tests.java -Dtest.class=HDF5Tests
 
 rm roothaan.h5
-#export MOE_SVL_LOAD=/home/roger/NetBeansProjects/MOEDivcon/svl
+export MOE_SVL_LOAD=/home/roger/NetBeansProjects/MOEDivcon/svl
 cp /home/roger/NetBeansProjects/MOEDivcon/dist/MOEDivcon.jar $PROJECT_PATH/linux-x86_64
 cp /home/roger/NetBeansProjects/MOEDivcon/svl/hdf5interface.svl $PROJECT_PATH/svl
 cp /home/roger/NetBeansProjects/JNIDivcon/dist/JNIDivcon.jar $PROJECT_PATH/linux-x86_64/lib
@@ -30,7 +32,7 @@ cp /home/roger/NetBeansProjects/JNIDivcon/dist/JNIDivcon.jar $PROJECT_PATH/linux
 #qbmoebatch -exec "qbTesth5Main['/home/roger/testing-grounds/s59/Moe interface/s59.pm6.sp.xml.h5']" -exit
 #qbmoebatch -exec "qbTestPDBtoH5['./roothaan.h5', '/home/roger/NetBeansProjects/OOBackbone/tests/examples/3FVA.pdb']" -exit
 #qbmoebatch -exec "qbTesth5Main['/home/roger/testing-grounds/Cl4Y2/roothaan.h5']" -exit
-qbmoebatch -exec "qbTesth5Main['/home/roger/testing-grounds/Lance/1LRI/1LRI-NMR.h5']" -exit
+qbmoebatch -exec "qbTesth5Main['/home/roger/testing-grounds/Josh/CSPtest/shadowfax/2YXJ_out.xml.h5']" -exit
 
 #qbmoe
 
